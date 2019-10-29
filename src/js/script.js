373cb95ae0d6ccf81bdc1ff6a3b688e76b8d09cc
alexpryp <alexpryp@gmail.com>
2019-10-29 23:32:32 +0200

    Hamburger-menu has been added

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+	// Gamburger menu animation
+	function displayMenu(event) {
+		$(".nav-menu").toggleClass("nav-menu-hide");
+		$(".first").toggleClass("first-active");
+		$(".second").toggleClass("second-active");
+		$(".third").toggleClass("third-active");
+		$(".fourth").toggleClass("fourth-active");
+	}
+
+	$(".gamburger-menu-wrapper").on('click', displayMenu);
+	$(".nav-menu").on('click', displayMenu);
+
+	//---------------------------------------------------------------------------
+	//Page component animation
+	
+}
